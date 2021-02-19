690f314130be6ad70782870690db24807153d3d1
Luiz Couto <luiz@pixfly.com>
2021-02-19 12:50:35 -0300

    Lambda warm

--- lambda.js
+++ lambda.js
@@ -1,4 +1,6 @@
-const awsServerlessExpress = require('aws-serverless-express');
+const awsServerlessExpress = require('aws-serverless-express')
+
+const warmer = require('lambda-warmer')
 
 // import the server factory function we created.
 const { createApp } = require('./server');
@@ -7,10 +9,26 @@ const { createApp } = require('./server');
 const appPromise = createApp();
 
 exports.nuxt = async (event, context) => {
-  // should return a fastify instance once resolved.
-  const app = await appPromise;
 
-  // proxies the request to our underlying fastify server.
-  return awsServerlessExpress.proxy(app.server, event, context, 'PROMISE')
-    .promise;
-};
+  // if a warming event
+  if (await warmer(event, { correlationId: context.awsRequestId, delay: 50 })) {
+
+    console.log('warmed request event...')
+
+    return 'warmed'
+
+  } else { // else proceed with handler logic
+
+    console.log('ordinary request event...')
+
+    // should return a fastify instance once resolved.
+    const app = await appPromise;
+
+    // proxies the request to our underlying fastify server.
+    return awsServerlessExpress.proxy(app.server, event, context, 'PROMISE').promise
+
+
+  }
+
+
+}
